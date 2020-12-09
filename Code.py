b72bd687a0b9ae05e0188c52d820fcdff2e3dbcf
FrabouletAlexis <a.fraboulet@rns.ecolescreatives.com>
2020-12-09 16:55:49 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -10,26 +10,26 @@ def mot_aleatoire ():
             mot = tableau_mot[i]    
     return mot
 def couleur (mot,mot_select):
-     for i in range (0,len(mot_select)):
+    for i in range (0,len(mot_select)):
         if (mot[i] == mot_select[i]):
             print(Back.RED+ mot_select[i])
         elif (mot[i] != mot_select[i]):
             print(Back.BLUE+ mot_select[i])
-def analyse_mot_select (mot_select,mot):
-    
-    
-mot= mot_aleatoire()
 
-# mot_select= input("Votre proposition ?")
-mot="carapace"
-lettre=input("lettre ?")
 
-def cherche_lettre (lettre,mot):
-    for i in range (0,len(mot)):
-        if (lettre==mot[i]):
-            return i
-    
-position=cherche_lettre(lettre,mot)
-print(position)
-def 
+# ------code------#
+tentative=0
+reussite=False   
+mot=mot_aleatoire()
+
+while (not(tentative == 9 or reussite == True)):
+
+    mot_select=input("Votre proposition ?")
+    print (couleur(mot,mot_select))
+    if (
+
+
+
+
+
 input()
