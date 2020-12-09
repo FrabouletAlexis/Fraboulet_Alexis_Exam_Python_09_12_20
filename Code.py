3c6a78446e49b0f6e02687cd0d7535662129a0f6
FrabouletAlexis <a.fraboulet@rns.ecolescreatives.com>
2020-12-09 15:46:49 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,18 @@
-uhhughu
+
+import random
+
+def mot_aleatoire ():
+    tableau_mot = ["RETARD","INOUIE","ABBAYE","CLAMER","DOPANT","ALARME","OVULER","MACHIN","ENFLER","ALCOOL"]
+    nb_mot=random.randrange(0,len(tableau_mot))
+    for i in range (0,len(tableau_mot)):
+        if (i == nb_mot):
+            mot = tableau_mot[i]    
+    return mot
+def analyse_mot_select (mot_select,mot):
+    
+    
+mot= mot_aleatoire()
+
+mot_select= int(input("Votre proposition ?"))
+
+input()
